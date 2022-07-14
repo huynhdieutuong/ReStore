895f5296112675c859320cdb2379759d2123060a
Tuong Huynh <41580247+huynhdieutuong@users.noreply.github.com>
2022-07-14 17:36:56 +0700

    Setup Theme: Dark mode

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,16 +1,30 @@
-import {CssBaseline} from '@mui/material'
+import {createTheme, CssBaseline, ThemeProvider} from '@mui/material'
 import {Container} from '@mui/system'
+import {useState} from 'react'
 import Catalog from '../../features/catalog/Catalog'
+import Header from './Header'
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false)
+  const handleThemeChange = () => setDarkMode(!darkMode)
+
+  const theme = createTheme({
+    palette: {
+      mode: darkMode ? 'dark' : 'light',
+      background: {
+        default: darkMode ? '#121212' : '#eaeaea',
+      },
+    },
+  })
+
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
-      <h1>Re-Store</h1>
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
       <Container>
         <Catalog />
       </Container>
-    </>
+    </ThemeProvider>
   )
 }
 
