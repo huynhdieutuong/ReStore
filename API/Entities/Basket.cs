9bef604fc6d1e2616911dd52a15a7de580d96ca7
Tuong Huynh <41580247+huynhdieutuong@users.noreply.github.com>
2022-07-17 21:41:22 +0700

    Add & Remove Item into Basket BE

---
+++ API/Entities/Basket.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+  public class Basket
+  {
+    public int Id { get; set; }
+    public string BuyerId { get; set; }
+
+    public List<BasketItem> Items { get; set; } = new();
+
+    public void AddItem(Product product, int quantity)
+    {
+      var item = Items.FirstOrDefault(item => item.ProductId == product.Id);
+      if (item == null)
+      {
+        Items.Add(new BasketItem { Quantity = quantity, Product = product });
+      }
+      else
+      {
+        item.Quantity += quantity;
+      }
+    }
+
+    public void RemoveItem(int productId, int quantity)
+    {
+      var item = Items.FirstOrDefault(item => item.ProductId == productId);
+      if (item == null) return;
+      item.Quantity -= quantity;
+      if (item.Quantity <= 0) Items.Remove(item);
+    }
+  }
+}
