94db7e06c7bb921c184c845ff7955328bd2a1831
Tuong Huynh <41580247+huynhdieutuong@users.noreply.github.com>
2022-08-19 13:38:38 +0700

    Payment Intent Stripe - BE

---
+++ API/Data/Migrations/20220819025247_PaymentIntentAdded.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    public partial class PaymentIntentAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "c72b2cb9-1a4e-4a13-8099-d38e87db12fd");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "f745a1ad-6277-4b23-9684-ee9a4d2a90c8");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentIntentId",
+                table: "Orders",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ClientSecret",
+                table: "Baskets",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentIntentId",
+                table: "Baskets",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "304f062e-8fb3-47b5-a2ef-f64fa369b998", "3da9fe46-31ee-4da3-91dd-602b3a41af57", "Admin", "ADMIN" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "b6aa0000-8040-4629-966b-c4ff2fb44e7c", "ba1c4a37-9883-468a-9f6a-653cedeb8698", "Member", "MEMBER" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "304f062e-8fb3-47b5-a2ef-f64fa369b998");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "b6aa0000-8040-4629-966b-c4ff2fb44e7c");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentIntentId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "ClientSecret",
+                table: "Baskets");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentIntentId",
+                table: "Baskets");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "c72b2cb9-1a4e-4a13-8099-d38e87db12fd", "7a502375-9337-451d-91c7-cf3865b9e4bc", "Member", "MEMBER" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "f745a1ad-6277-4b23-9684-ee9a4d2a90c8", "143e8ff3-8822-451c-ae26-ba06e0154953", "Admin", "ADMIN" });
+        }
+    }
+}
--- API/Extensions/BasketExtensions.cs
+++ API/Extensions/BasketExtensions.cs
@@ -18,6 +18,8 @@ namespace API.Extensions
       {
         Id = basket.Id,
         BuyerId = basket.BuyerId,
+        PaymentIntentId = basket.PaymentIntentId,
+        ClientSecret = basket.ClientSecret,
         Items = basket.Items.Select(item => new BasketItemDto
         {
           ProductId = item.ProductId,
